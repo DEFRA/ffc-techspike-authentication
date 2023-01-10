3025434959a8ffdf7a146a59f11059cd72908862
steve-dickinson <steve.dickinson@defra.gov.uk>
2023-01-10 11:44:51 +0000

    Update https switch

--- app/server.js
+++ app/server.js
@@ -4,18 +4,6 @@ const catbox = config.useRedis
   ? require('@hapi/catbox-redis')
   : require('@hapi/catbox-memory')
 const cacheConfig = config.useRedis ? config.cache.options : {}
-const fs = require('fs')
-const Path = require('path')
-const Http2 = require('http2')
-
-const readContentsFromFile = filePath => fs.readFileSync(Path.join(__dirname, filePath))
-
-const serverOptions = {
-  key: readContentsFromFile('localhost-privkey.pem'),
-  cert: readContentsFromFile('localhost-cert.pem')
-}
-
-const listener = Http2.createSecureServer(serverOptions)
 
 async function createServer () {
   const serverOptions = {
@@ -40,6 +28,18 @@ async function createServer () {
   }
   console.log('config.useHttps', config.useHttps)
   if (config.useHttps) {
+    const fs = require('fs')
+    const Path = require('path')
+    const Http2 = require('http2')
+
+    const readContentsFromFile = filePath => fs.readFileSync(Path.join(__dirname, filePath))
+
+    const serverOptions = {
+      key: readContentsFromFile('localhost-privkey.pem'),
+      cert: readContentsFromFile('localhost-cert.pem')
+    }
+
+    const listener = Http2.createSecureServer(serverOptions)
     serverOptions.listener = listener
   }
 
